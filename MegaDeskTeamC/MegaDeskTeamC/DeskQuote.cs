bb2f2dc6c6f6a853da5a680b90c916b98cdc9fd6
riqui.tellez <riqui.tellez@optimissa.com>
2020-10-16 00:57:34 +0200

    Group meeting 3 - Reading rush days from file

--- MegaDeskTeamC/MegaDeskTeamC/DeskQuote.cs
+++ MegaDeskTeamC/MegaDeskTeamC/DeskQuote.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,118 @@ namespace MegaDeskTeamC
         public string RushDays { get; set; }
         public int TotalPrice { get; set; } 
         public string Date { get; set; }
+
+        public const int BASESURFACE = 1000;
+        public const int OVERSURFACE = 1;
+        public const int LARGESURFACE = 2000;
+
+
+        public int PriceRush()
+        {
+            int result = 0;
+	        int[,] rushOrderPrices = new int[3, 3];
+	        string[] priceList = new string[9];
+	        priceList = ReadRushOrderPrices();
+	        int counter = 0;
+		        while (counter < 9)
+		        {
+			        for (int y = 0; y < 3; y++)
+			        {
+				        for (int x = 0; x < 3; x++)
+				        {
+					        rushOrderPrices[y, x] = int.Parse(priceList[counter]);
+					        counter++;
+				        }
+			        }
+		        }
+
+            if(RushDays == "3")
+            { 
+                if(DeskSurfaceArea() > 2000)
+                {
+                    result = rushOrderPrices[0,2];
+                }
+
+                else if(DeskSurfaceArea() >= 1000 && DeskSurfaceArea() <= 2000)
+                {
+                    result = rushOrderPrices[0,1];
+                }
+
+                else
+                {
+                    result = rushOrderPrices[0,0];
+                }
+
+            }
+
+            else if(RushDays == "5")
+            { 
+                if(DeskSurfaceArea() > 2000)
+                {
+                    result = rushOrderPrices[1,2];
+                }
+
+                else if(DeskSurfaceArea() >= 1000 && DeskSurfaceArea() <= 2000)
+                {
+                    result = rushOrderPrices[1,1];
+                }
+
+                else
+                {
+                    result = rushOrderPrices[1,0];
+                }
+
+            }
+
+            else if(RushDays == "7" )
+            {
+                if(DeskSurfaceArea() > 2000)
+                {
+                    result = rushOrderPrices[2,2];
+                }
+
+                else if(DeskSurfaceArea() >= 1000 && DeskSurfaceArea() <= 2000)
+                {
+                    result = rushOrderPrices[2,1];
+                }
+
+                else
+                {
+                    result = rushOrderPrices[2,0];
+                }
+            }
+
+
+            return result;
+        }
+
+        public static string[] ReadRushOrderPrices()
+        {
+	        string[] priceList = new string[9];
+	        try
+	        {
+		        var filePath = @"../../data/rushOrderPrices.txt";
+		        StreamReader reader = new StreamReader(filePath);
+		        int i = 0;
+		        while (reader.EndOfStream == false)
+		        {
+			        string line = reader.ReadLine();
+			        priceList[i] = line;
+			        i++;
+		        }
+
+		        reader.Close();
+	
+	        }
+	        catch (Exception e)
+	        {
+                throw new Exception(e.Message);
+	        }
+	        return priceList;
+        }
+
+        public static int[,] rushOrderPrices = new int[3,3];
+
 
         public string getDate()
 
@@ -63,7 +176,9 @@ namespace MegaDeskTeamC
             }
         }
 
-        public int PriceRush()
+
+
+        /*public int PriceRush()
         {
             int addedPrice = 0;
             if (RushDays == "3")
@@ -123,7 +238,7 @@ namespace MegaDeskTeamC
             {
                 return addedPrice;
             }
-        }
+        }*/
 
         public int GetTotal()
         {
